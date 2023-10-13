12b06531226fb849eba8e856c2c3a9d105dac5c2
Siddharth Singh Kushwaha <siddharthmnbv@gmail.com>
2023-10-13 12:25:16 +0530

    env

--- app/components/Modals/LoginModal.tsx
+++ app/components/Modals/LoginModal.tsx
@@ -1,6 +1,4 @@
-'use client';
 
-import axios from 'axios';
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { useCallback, useState } from 'react';
